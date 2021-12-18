5e130c93f2950992d34b3b6a430498bf317d41e5
david-allison12 <dgreenal@gmail.com>
2021-12-17 19:19:40 -0500

    Redeploying

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(cors())
 require('dotenv').config()
 
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID
 const authToken = process.env.TWILIO_AUTH_TOKEN
 const twilioPhone = process.env.TWILIO_PHONE_NUMBER
